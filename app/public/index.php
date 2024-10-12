fcc389c2a0a205c6bd5bd3b98a60586e4d3af516
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2024-10-12 11:51:28 +0200

    Front Controller

---
+++ app/public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+const VIEWSPATH = '../Views';
+
+$request = $_SERVER['REQUEST_URI'];
+$route = strtok($request, '?');
+$queryString = strtok('');
+
+$queryParams = [];
+if (!empty($queryString)) {
+    foreach (explode('&', $queryString) as $value) {
+        list($param, $val) = explode('=', $value);
+        $queryParams[$param] = $val;
+    }
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+    case 'GET':
+        include_once VIEWSPATH . '/Fixed/head.php';
+        include_once VIEWSPATH . '/Fixed/header.php';
+
+        if ($route == '/')
+            $route = '/index';
+
+        $page = VIEWSPATH . $route . '.php';
+        if (file_exists($page))
+            include_once $page;
+        else
+            include_once VIEWSPATH . '/404.php';
+
+        include_once VIEWSPATH . '/Fixed/footer.php';
+        break;
+
+    case 'POST':
+        break;
+}
