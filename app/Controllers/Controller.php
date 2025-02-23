6334ae95967a0ec031c84e51fbee56650aaa45fc
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-02-23 20:12:03 +0100

    Added query parameters and json property to controllers

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,4 +2,28 @@
 
 namespace App\Controllers;
 
-abstract class Controller {}
+abstract class Controller
+{
+    protected array $queryParameters = [];
+    protected array | object $json;
+
+    public function __construct()
+    {
+        $queryString = explode('?', $_SERVER["REQUEST_URI"])[1];
+        if (isset($queryString)) {
+            foreach (explode('&', $queryString) as $value) {
+                if (!empty($value)) {
+                    list($param, $val) = explode('=', $value);
+                    $this->queryParameters[$param] = $val;
+                }
+            }
+        }
+
+        $data = json_decode(file_get_contents('php://input'));
+        if ($data === null) {
+            $this->json = new \stdClass();
+        } else {
+            $this->json = $data;
+        }
+    }
+}
