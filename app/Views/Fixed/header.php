bdb56ef7ce89dd3ce4bd8b1671a0f1a931468508
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-03-02 00:31:22 +0100

    Removed unsused links in header

--- app/Views/Fixed/header.php
+++ app/Views/Fixed/header.php
@@ -2,8 +2,6 @@
     <div class="container d-flex justify-content-center py-3">
         <ul class="nav nav-pills">
             <li class="nav-item"><a href="radio" class="nav-link">Radio</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Vehicle</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Character</a></li>
         </ul>
     </div>
 </header>
