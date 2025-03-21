25368deaafc3901629eb9055ebb540358b113589
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-03-21 19:18:22 +0100

    New database schema

--- utils/setup_database.sql
+++ utils/setup_database.sql
@@ -1,34 +1,39 @@
 START TRANSACTION;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `games`
---
-
 CREATE TABLE `games` (
   `id` int(10) NOT NULL,
   `radio_id` int(2) NOT NULL,
   `song_id` int(3) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `origin_id` int(1) NOT NULL,
+  `giver_id` int(2) NOT NULL,
   `game_date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `missions` (
+  `id` int(3) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `origin_id` int(1) NOT NULL,
+  `giver_id` int(2) NOT NULL,
+  `description` varchar(1024) NOT NULL,
+  `objective` varchar(1024) NOT NULL,
+  `reward` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
---
--- Table structure for table `radio_stations`
---
+CREATE TABLE `mission_givers` (
+  `id` int(2) NOT NULL,
+  `name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+CREATE TABLE `mission_origins` (
+  `id` int(1) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 CREATE TABLE `radio_stations` (
   `id` int(2) NOT NULL,
   `name` varchar(18) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `songs`
---
 
 CREATE TABLE `songs` (
   `id` int(3) NOT NULL,
@@ -38,69 +43,66 @@ CREATE TABLE `songs` (
   `video_id` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `games`
---
 ALTER TABLE `games`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_RADIO_ID` (`radio_id`),
-  ADD KEY `FK_SONG_ID` (`song_id`);
+  ADD KEY `FK_SONG_ID` (`song_id`),
+  ADD KEY `FK_TITLE` (`title`),
+  ADD KEY `FK_ORIGIN_ID` (`origin_id`),
+  ADD KEY `FK_GIVER_ID` (`giver_id`);
 
---
--- Indexes for table `radio_stations`
---
+ALTER TABLE `missions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IX_TITLE_UNIQUE` (`title`),
+  ADD KEY `FK_ORIGIN_ID` (`origin_id`),
+  ADD KEY `FK_GIVER_ID` (`giver_id`);
+
+ALTER TABLE `mission_givers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IX_NAME_UNIQUE` (`name`);
+
+ALTER TABLE `mission_origins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IX_NAME_UNIQUE` (`name`);
+
 ALTER TABLE `radio_stations`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `IX_NAME_UNIQUE` (`name`);
 
---
--- Indexes for table `songs`
---
 ALTER TABLE `songs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `IX_VIDEO_ID_UNIQUE` (`video_id`) USING BTREE,
   ADD KEY `FK_RADIO_ID` (`radio_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `games`
---
 ALTER TABLE `games`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `radio_stations`
---
+ALTER TABLE `missions`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `mission_givers`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `mission_origins`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `radio_stations`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `songs`
---
 ALTER TABLE `songs`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `games`
---
 ALTER TABLE `games`
   ADD CONSTRAINT `games_ibfk_1` FOREIGN KEY (`radio_id`) REFERENCES `radio_stations` (`id`),
   ADD CONSTRAINT `games_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`);
 
---
--- Constraints for table `songs`
---
+ALTER TABLE `missions`a
+  ADD CONSTRAINT `missions_ibfk_1` FOREIGN KEY (`origin_id`) REFERENCES `mission_origins` (`id`),
+  ADD CONSTRAINT `missions_ibfk_2` FOREIGN KEY (`giver_id`) REFERENCES `mission_givers` (`id`);
+
 ALTER TABLE `songs`
   ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`radio_id`) REFERENCES `radio_stations` (`id`);
 COMMIT;
