becac3e544cc9367ce829d4d69e62ceb89939374
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-03-22 16:49:28 +0100

    Added mission minigame view

--- app/Views/Fixed/header.php
+++ app/Views/Fixed/header.php
@@ -1,7 +1,8 @@
 <header>
     <div class="container d-flex justify-content-center py-3">
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="radio" class="nav-link">Radio</a></li>
+            <li class="nav-item mx-2"><a href="mission" class="nav-link">Mission</a></li>
+            <li class="nav-item mx-2"><a href="radio" class="nav-link">Radio</a></li>
         </ul>
     </div>
 </header>
---
+++ app/Views/mission.php
@@ -0,0 +1,79 @@
+<div class="container-md">
+    <div id="mission-title" class="mission card my-3">
+        <div class="card-body">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Objective</td>
+                        <td>Origin</td>
+                        <td>Giver</td>
+                        <td>Reward</td>
+                    </tr>
+                    <tr>
+                        <td class="title">?</td>
+                        <td class="description">?</td>
+                        <td class="objective">?</td>
+                        <td class="origin">?</td>
+                        <td class="giver">?</td>
+                        <td class="reward">?</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div id="mission-origin" class="mission card my-3">
+        <div class="card-body">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Objective</td>
+                        <td>Origin</td>
+                        <td>Giver</td>
+                        <td>Reward</td>
+                    </tr>
+                    <tr>
+                        <td class="title">?</td>
+                        <td class="description">?</td>
+                        <td class="objective">?</td>
+                        <td class="origin">?</td>
+                        <td class="giver">?</td>
+                        <td class="reward">?</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div id="mission-giver" class="mission card my-3">
+        <div class="card-body">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Objective</td>
+                        <td>Origin</td>
+                        <td>Giver</td>
+                        <td>Reward</td>
+                    </tr>
+                    <tr>
+                        <td class="title">?</td>
+                        <td class="description">?</td>
+                        <td class="objective">?</td>
+                        <td class="origin">?</td>
+                        <td class="giver">?</td>
+                        <td class="reward">?</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="container-sm">
+    <p class="text-center mt-3 mb-0">Time to next game:</p>
+    <p id="timer" class="text-center"></p>
+</div>
+<script src="assets/js/main.js"></script>
